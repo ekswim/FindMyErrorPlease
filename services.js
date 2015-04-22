e7f308d4648b8e57e658eaf66489885f24ac4dc6
Evan Kraus <evan.kraus017@gmail.com>
2015-04-22 16:08:18 -0500

    Create services.js

---
+++ services.js
@@ -0,0 +1,48 @@
+angular.module('songhop.services', [])
+
+.factory('User', function() {
+	
+	var o = {
+		favorites: []
+	}
+	
+	o.addSongToFavorites = function(song) {
+		if (!song) return false;
+		
+		o.favorites.unshift(song);
+	}
+	
+	o.removeSongFromFavorites = function(song, index) {
+		if (!song) return false;
+		
+		o.favorites.splice(index, 1);
+}
+	
+	return o;
+})
+
+.factory('Recommendations', function($http, SERVER) {
+	var o = {
+		queue: []
+	};
+	
+	o.getNextSongs = function() {
+		return $http({
+			method: 'GET',
+			url: SERVER.url + '/recommendations'
+		}).success(function(data) {
+			o.queue = o.queue.contact(data);
+		});
+	}
+	
+	o.nextSong = function() {
+		o.queue.shift();
+		
+		if (o.queue.length <= 3) {
+			o.getNextSongs();
+		}
+	}
+	
+	return o;
+
+});
